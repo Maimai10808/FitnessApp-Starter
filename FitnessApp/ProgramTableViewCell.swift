a6f570d90e26f5d33789f2cbf07ebe4dcdb59c73
Maimai10808 <331686549@qq.com>
2025-07-15 02:03:44 +0800

    4. Program Table View Cell Class

--- FitnessApp/ProgramTableViewCell.swift
+++ FitnessApp/ProgramTableViewCell.swift
@@ -9,19 +9,40 @@ import UIKit
 
 class ProgramTableViewCell: UITableViewCell {
 
+    static let identifer = "ProgramTableViewCell"
+    
     @IBOutlet weak var titleLabel   : UILabel!
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var caloriesLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        let gradientLayer = CAGradientLayer()
+        
+        let colorTop    = UIColor.primary  .cgColor
+        let colorBottom = UIColor.secondary.cgColor
+        
+        gradientLayer.colors        = [colorTop, colorBottom]
+        gradientLayer.locations     = [0.0, 1.0]
+        gradientLayer.bounds        = containerView.bounds
+        gradientLayer.cornerRadius  = 8
+        containerView.layer.insertSublayer(gradientLayer, at: 0)
+        containerView.clipsToBounds = true
+        
+        titleLabel   .font = UIFont.style(.h1)
+        durationLabel.font = UIFont.style(.caption)
+        caloriesLabel.font = UIFont.style(.caption)
+        
+        selectionStyle     = .none
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        containerView.layer.cornerRadius = 8
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
