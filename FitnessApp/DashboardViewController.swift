e4d61de178ff1f26d54f4fb2a55ddbd546035cc3
Maimai10808 <331686549@qq.com>
2025-07-15 15:03:08 +0800

    7.Workout View Controller

--- FitnessApp/DashboardViewController.swift
+++ FitnessApp/DashboardViewController.swift
@@ -17,14 +17,22 @@ class DashboardViewController: UIViewController {
         Program.advancedMockProgram
     ]
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "WorkoutSegue" {
+            let destinationVC     = segue.destination as! WorkoutViewController
+            let selectedProgram   = sender            as! Program
+            destinationVC.program = selectedProgram
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let nibName   = "ProgramTableViewCell"
         
         
-        tableView.dataSource = self
-        tableView.delegate   = self
+        tableView.dataSource     = self
+        tableView.delegate       = self
         
         tableView.register(UINib(nibName: nibName, bundle: nil),
                            forCellReuseIdentifier: ProgramTableViewCell.identifer)
@@ -57,8 +65,39 @@ extension DashboardViewController: UITableViewDataSource {
 // MARK: TableView Delegate
 extension DashboardViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedProgram = programs[indexPath.row]
+        performSegue(withIdentifier: "WorkoutSegue", sender: selectedProgram)
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 180
     }
 }
+
+/*
+ 
+ [DashboardViewController]
+          ↓
+ 点击 cell (didSelectRowAt)
+          ↓
+ performSegue("workoutSegue", sender: selectedProgram)
+          ↓
+ prepare(for: segue, sender:)
+          ↓
+ destinationVC.program = sender as? Program
+          ↓
+ [WorkoutViewController]
+         ↓
+ 使用 program 进行 UI 展示 / workout 逻辑执行
+ 
+ 在 iOS 开发中，sender 的中文意思是：
+
+ 发送者，即触发这个操作的“源对象”或“传递的数据”。 触发事件的对象 / 传递的数据
+
+ 
+ @IBAction func buttonTapped(_ sender: UIButton) {
+     // sender 就是触发这个方法的按钮本身
+ }
+ */
 
---
+++ FitnessApp/WorkoutViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutViewController.swift
+//  FitnessApp
+//
+//  Created by mac on 7/15/25.
+//
+
+import UIKit
+
+class WorkoutViewController: UIViewController {
+    
+    var program: Program!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
