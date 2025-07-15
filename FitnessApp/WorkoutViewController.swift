bcae0d5b381750d32b74128f00c9ec7ae242f003
Maimai10808 <331686549@qq.com>
2025-07-15 18:41:04 +0800

    9. Added Lottie Animation

--- FitnessApp/WorkoutViewController.swift
+++ FitnessApp/WorkoutViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Lottie
 
 class WorkoutViewController: UIViewController {
     
     @IBOutlet weak var ringView: RingView!
+    
+    var animationView: LottieAnimationView!
     
     var program: Program!
 
@@ -19,7 +22,22 @@ class WorkoutViewController: UIViewController {
         ringView.delegate = self
         
         ringView.progress = 0.0
-        // Do any additional setup after loading the view.
+        
+        animationView   = LottieAnimationView(name: "jumpingjack")
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.loopMode    = .loop
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        
+        view.addSubview(animationView)
+        
+        // ➤ 添加居中与固定大小约束
+            NSLayoutConstraint.activate([
+                animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                animationView.widthAnchor  .constraint(equalToConstant: 150),
+                animationView.heightAnchor .constraint(equalToConstant: 150)
+            ])
     }
     
     override func viewDidAppear(_ animated: Bool) {
